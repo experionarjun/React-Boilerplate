253598f1010246bf1e0aebc6f06f5050b56b3944
Arjun Sreedhar <arjun.sreedhar@experionglobal.com>
2018-06-21 12:02:08 +0530

    init structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webpack Config
+Just a test repo to show webpack config for a react project.
+
+Install dependencies
+```
+yarn install
+```
+Run dev
+```
+yarn start
+```
+To take build
+```
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var config = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public',
+        inline: true,
+        historyApiFallback: true,
+        port: 3200,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/, // search for js files 
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'] // use es2015 and react
+                }
+            }
+        ]
+    }
+}
+
+module.exports = config;
